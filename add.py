9828ca458bcd6239aed3e628e6c1a5956f95ef22
unknown <vishali.nagathevan@gmail.com>
2023-03-12 20:23:03 +0530

    basic add program

---
+++ add.py
@@ -0,0 +1,31 @@
+no =1
+#no= no+1
+no =1+1
+print(no)
+
+num = 2
+num += 5
+print(num)
+
+name__ ="vishali ", " pappa"*4 ,77 *2
+print (name__)
+
+a =[10,111,112,24]
+print(len(a))
+print(a[2])
+   
+   #output
+
+place=["maduri","is a","temple","city"]
+for i in place :
+ print(place )#end=" ",)
+
+xx=12.05
+yy=12E455677889
+zz=578989.4e76
+print(type(xx),type(yy),type(zz))
+
+import random
+nunmber =11
+print(random.randrange(nunmber))
+
