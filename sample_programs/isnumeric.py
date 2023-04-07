338cac179783a51032029ddedbd93e0a58956ff2
unknown <vishali.nagathevan@gmail.com>
2023-04-07 22:33:19 +0530

    isnumeric  program

--- sample_programs/isnumeric.py
+++ sample_programs/isnumeric.py
@@ -5,10 +5,9 @@ print("The String Is Numeric :", str_value.isnumeric())
 str1_value ="1AB44567"
 print("The String Is Numeric :", str1_value.isnumeric())
 
-# without using in-build method 
-Str_NumericValue="12345"
-
-if i==Str_NumericValue:
+# without using in-build method    ????
+Str_NumericValue="12hhg345"
+if Str_NumericValue.isnumeric:
     print("YES THIS IS NUMERIC VALUE ")
 else:
     print("NO THIS IS NOT NUMERIC VALUE")    
