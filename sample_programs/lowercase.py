6e6f6dfd4a2680d2fdfb4692e2573a2418738fb9
unknown <vishali.nagathevan@gmail.com>
2023-04-07 22:29:25 +0530

    Lowercase program

--- sample_programs/lowercase.py
+++ sample_programs/lowercase.py
@@ -8,6 +8,7 @@ for i in lower_Case:
 print()         
 
 Name ="VishaLI"
-Lower_Case_Latters =[i for i in Name if i.islower()]  
-print(Lower_Case_Latters) 
-print(Lower_Case_Latters) 
+Lower_Case_Latters =[i for i in Name if i.islower() ]  
+upper_Case_Latters =[i for i in Name if i.isupper() ]  
+print("Lower_Case_Latter : ",Lower_Case_Latters) 
+print("upper_Case_Latters : ",upper_Case_Latters) 
